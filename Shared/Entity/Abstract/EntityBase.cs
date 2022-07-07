419bd2cb69e8794f017eb0dfe719bc920b782c8a
Deniz Duman <ce.denizduman2@gmail.com>
2022-07-07 15:51:14 +0300

    Add project files.

---
+++ Shared/Entity/Abstract/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Entity.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int ID { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+    }
+}
