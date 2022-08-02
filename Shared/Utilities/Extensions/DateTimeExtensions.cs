d9aea31c052dcc8eb68dc9ca132ff34182c119f4
Deniz Duman <ce.denizduman2@gmail.com>
2022-08-02 16:53:59 +0300

    user için image-1

---
+++ Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime dateTime)
+        {
+            return $"{dateTime.Year}/{dateTime.Month}/{dateTime.Day}-{dateTime.Hour}:{dateTime.Minute}:{dateTime.Second}";
+        }
+    }
+}
--- WebUI/Areas/Admin/Controllers/UserController.cs
+++ WebUI/Areas/Admin/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Entity.Concrete.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Shared.Utilities.Extensions;
 using Shared.Utilities.Results.ComplexTypes;
 
 namespace WebUI.Areas.Admin.Controllers
@@ -11,10 +12,11 @@ namespace WebUI.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<User> _userManager;
-
-        public UserController(UserManager<User> userManager)
+        private readonly IWebHostEnvironment _env;
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env)
         {
             _userManager = userManager;
+            _env = env;
         }
 
         public async Task<IActionResult> List()
@@ -31,6 +33,27 @@ namespace WebUI.Areas.Admin.Controllers
         public IActionResult Add()
         {
             return PartialView("_UserAddPartial");
+        }
+
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            string wwwroot = _env.WebRootPath;
+
+            //denizduman
+            // string fileName = Path.GetFileNameWithoutExtension(userAddDto.Picture.FileName);
+
+            // png||jpg
+            string fileExtension = Path.GetExtension(userAddDto.Picture.FileName);
+
+            DateTime dateTime = DateTime.Now;
+
+            string fileName = $"{userAddDto.UserName}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path = Path.Combine($"{wwwroot}/Admin/dz-img/user-img",fileName);
+            await using (var stream = new FileStream(path,FileMode.Create))
+            {
+                await userAddDto.Picture.CopyToAsync(stream);
+            }
+            return fileName; 
         }
     }
 }
