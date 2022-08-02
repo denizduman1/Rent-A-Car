bdfee5109056e6b6b16e7e89153b9ee601e9cabd
Deniz Duman <ce.denizduman2@gmail.com>
2022-08-02 11:23:55 +0300

    Kullanıcı listeleme gerçekleştirildi

---
+++ WebUI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Entity.Concrete;
+using Entity.Concrete.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared.Utilities.Results.ComplexTypes;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            });
+        }
+    }
+}
