fde2dd8fc10ef2aac64bb140780343996744dc46
denizduman1 <ce.denizduman2@gmail.com>
2022-08-08 21:24:52 +0300

    admin menu dinamikleştirildi

---
+++ WebUI/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Entity.Concrete;
+
+namespace WebUI.Areas.Admin.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
