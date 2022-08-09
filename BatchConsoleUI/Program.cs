07b8e70fa298eefa274f3bd9e1a4374390831c9e
Deniz Duman <ce.denizduman2@gmail.com>
2022-08-09 16:46:29 +0300

    batch -1

---
+++ BatchConsoleUI/Program.cs
@@ -0,0 +1,6 @@
+﻿using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Services.Concrete;
+
+//PaymentManager paymentManager = new PaymentManager(new EfUnitOfWork(new RentCarContext()),);
+
